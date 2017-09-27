165701f3019bf9ff55a04f699b69e4044d5e6f43
kazuki kaneshiro <ktmlike3214@gmail.com>
2017-09-27 18:27:05 +0900

    敵の追尾と増殖の処理を追加、アセットの追加

---
+++ rpg/Assets/Scripts/NewRandomCharacters.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// 
+/// </summary>
+
+using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]  
+
+//Name of class must be name of file as well
+
+public class NewRandomCharacters : MonoBehaviour {
+
+	//発生するオブジェクトをInspectorから指定する用
+	public GameObject spawnObject;
+
+	void Start () {
+
+		// コルーチンの開始
+		StartCoroutine ("Spawn");
+
+//		// コルーチンの終了
+//		StopCoroutine("Spawn");
+	}
+
+	IEnumerator Spawn() {
+
+		for(int i = 0; i < 10; i++) {
+			//自分をつけたオブジェクトの位置に、発生するオブジェクトをインスタンス化して生成する
+			Instantiate(spawnObject, transform.position, Quaternion.identity);
+			yield break;
+		}
+	}
+}
